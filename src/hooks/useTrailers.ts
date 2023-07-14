6df9286a049ee8b15644cc9357267d7c28416eae
snicol21 <spencer.nicol@gmail.com>
2023-07-14 13:48:14 -0600

    build game screenshots

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -4,6 +4,7 @@ import APIClient from '../services/api-client';
 
 const useTrailers = (gameId: number) => {
   const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+
   return useQuery({
     queryKey: ['trailers', gameId],
     queryFn: apiClient.getAll,
